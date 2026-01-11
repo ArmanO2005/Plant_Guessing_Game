a50e4efdc23600804e5e192bb96acd06b81beec2
ArmanO2005 <cyclanthaceae@berkeley.edu>
2026-01-10 22:19:38 -0800

    built

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/Plant_Guessing_Game",
+  assetPrefix: "/Plant_Guessing_Game/",
   trailingSlash: true,
   images: {
     unoptimized: true,
